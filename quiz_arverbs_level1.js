30446c38aaa14b46c507bdcebbc8c961e92aca5c
meili <138686586+hummingbird0-0@users.noreply.github.com>
2024-02-20 22:38:41 +0900

    ページとナビゲーションバーを追加しました

---
+++ quiz_arverbs_level1.js
@@ -0,0 +1,18 @@
+const quizDataEsp = [
+    {
+      question: 'yo | estudiar',
+      1: 'estudia',
+      2: 'estudio',
+      3: 'estudias',
+      correct: 2
+    },
+    {
+      question: 'tú | hablar',
+      1: '',
+      2: '',
+      3: '',
+      correct: ''
+    }
+  ];
+  
+  export { quizDataJp }
