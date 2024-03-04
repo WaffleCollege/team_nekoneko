dfed6ace62d1e7ac27cd3afd7fcf64288d36e84f
aoi0108 <hira.aoi.18@gmail.com>
2024-03-04 22:33:41 +0900

    test

---
+++ public/conjugationList.js
@@ -0,0 +1,20 @@
+//音と合わせるためにconjugationListの単語の数は５個以内にする！
+
+export const conjugationList = {
+  lv1: [
+    ["VIVIR | 住む", "vivo", "vives", "vive", "vivimos", "vivís", "viven"],
+    ["COMER | 食べる", "como", "comes", "come", "comemos", "coméis", "comen"],
+  ["ESTUDIAR | 勉強する]","estudio", "estudias","estudia","estudiamos","estudiáis", "estudian"],
+    ["VER | 見る", "veo", "ves",'ve', 'vemos', "veis", "ven" ],
+    ["ESCUCHAR | 歌う", "escucho", "escuchas", "escucha", "escuchamos", "escucháis", "escuchan"]
+  ],
+  lv2:[
+    ["CENAR | 夜ご飯を食べる", "ceno", "cenas", "cena", "cenamos", "cenáis", "cenan"],
+    ["IR | 行く", "voy", "vas", "va", "vamos", "váis", "van"],
+  ["COMPRAR | 買う","compro", "compras","compra","compramos","compráis", "compran"],
+    ["ASISTIR | 参加する", "asisto", "asistes",'asiste', 'asistimos', "asistís", "asisten" ],
+    ["VIAJAR | 旅をする", "viajo", "viajas", "viaja", "viajamos", "viajáis", "viajan"]
+
+  ]};
+
+
